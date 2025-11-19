39e5cbe82fd090e27533f37e4902c07efba211db
Mourad <mouradface1998@gmail.com>
2025-11-19 17:06:27 +0100

    Updates

--- assets/RedirectArticle-6mXazEAi.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-VmEDPuHV.js";async function t(){return r("/")}function n(){return null}export{n as default,t as loader};
---
+++ assets/RedirectCategory-I-vQbNC1.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index-D0W1Llbe.js";async function t(){return r("/")}function n(){return null}export{n as default,t as loader};
